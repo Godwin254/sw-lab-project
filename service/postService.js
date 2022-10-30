8981dbee1205d2d8a9d252e6bc5072eb310145ef
Godwin254 <godwinbee15@gmail.com>
2022-10-31 00:32:32 +0300

    feat: create User and Post service

---
+++ service/postService.js
@@ -0,0 +1,61 @@
+const Post = require('../model/Post');
+
+//get all posts from db
+const getAllPosts = async () => {
+    try{
+        const posts = await Post.find();
+        return posts;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+//get post by id from db
+const getOnePost = async (postId) => {
+    try{
+        const post = await Post.findById(postId);
+        return post;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+//create new post
+const createPost = async (newPost) => {
+    try{
+        const post = await Post.create(newPost);
+        await post.save();
+        return post;
+    }catch(err){
+        console.log(err);
+    }
+
+}
+
+//update post by id
+const updatePost = async (postId, updatedPost) => {
+    try{
+        const post = await Post.findByIdAndUpdate(postId, updatedPost, {new: true});
+        return post;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+//delete post by id
+const deletePost = async (postId) => {
+    try{
+        const post = await Post.findByIdAndDelete(postId);
+        return post;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = {
+    getAllPosts,
+    getOnePost,
+    createPost,
+    updatePost,
+    deletePost
+}
---
+++ service/userService.js
@@ -0,0 +1,62 @@
+
+const User = require('../model/User');
+
+//get all users from db
+const getAllUsers = async () => {
+    try{
+        const users = await User.find();
+        return users;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+//get user by id from db
+const getOneUser = async (userId) => {
+    try{
+        const user = await User.findById(userId);
+        return user;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+//create new user
+const createUser = async (newUser) => {
+    try{
+        const user = await User.create(newUser);
+        await user.save();
+        return user;
+    }catch(err){
+        console.log(err);
+    }
+
+}
+
+//update user by id
+const updateUser = async (userId, updatedUser) => {
+    try{
+        const user = await User.findByIdAndUpdate(userId, updatedUser, {new: true});
+        return user;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+//delete user by id
+const deleteUser = async (userId) => {
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        return user;
+    }catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser
+}
