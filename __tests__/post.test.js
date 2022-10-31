c24cdd1fb23b1075e1a11a78e0c9db22b445b552
Godwin254 <godwinbee15@gmail.com>
2022-10-31 13:56:30 +0300

    tests: add unit tests for posts and users endpoints

---
+++ __tests__/post.test.js
@@ -0,0 +1,48 @@
+//test postRouter.js
+const mongoose = require('mongoose');
+const { describe } = require('node:test');
+const request = require('supertest');
+
+const app = require('../app');
+require('dotenv').config();
+
+//connect to db before tests
+beforeAll(async () => {
+    await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+});
+
+//close db connection after tests
+afterAll(async () => {
+    await mongoose.connection.close();
+});
+
+//test post routes (endpoints)
+describe('GET /api/posts', () => {
+    it('should return 200 OK', () => {
+        return request(app).get('/api/posts').expect(200);
+    });
+});
+
+describe('GET /api/posts/:id', () => {
+    it('should return 200 OK', () => {
+        return request(app).get('/api/posts/5f4a1f7d8e3f1a3a3c3e3e3e').expect(200);
+    });
+});
+
+describe('POST /api/posts', () => {
+    it('should return 201 OK', () => {
+        return request(app).post('/api/posts').expect(201);
+    });
+});
+
+describe('PATCH /api/posts/:id', () => {
+    it('should return 200 OK', () => {
+        return request(app).patch('/api/posts/5f4a1f7d8e3f1a3a3c3e3e3e').expect(200);
+    });
+});
+
+describe('DELETE /api/posts/:id', () => {
+    it('should return 200 OK', () => {
+        return request(app).delete('/api/posts/5f4a1f7d8e3f1a3a3c3e3e3e').expect(200);
+    });
+});
