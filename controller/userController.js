7236315a0016bdb55c727ffdf0d9c1ac75d982f0
Godwin254 <godwinbee15@gmail.com>
2022-10-31 13:06:37 +0300

    feat: add controller logic for User and Posts

---
+++ controller/userController.js
@@ -0,0 +1,60 @@
+const userServices = require('../services/userServices');
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await userServices.getAllUsers();
+        res.status(200).json(users);
+    }catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const getOneUser = async (req, res) => {
+    try {
+        const {userId} = req.params;
+        const user = await userServices.getOneUser(userId);
+        res.status(200).json(user);
+    }catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const createUser = async (req, res) => {
+    try {
+        const newUser = req.body;
+        const user = await userServices.createUser(newUser);
+        res.status(201).json(user);
+    }catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        const {userId} = req.params;
+        const updatedUser = req.body;
+
+        const user = await userServices.updateUser(userId, updatedUser);
+        res.status(200).json(user);
+    }catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const {userId} = req.params;
+        const user = await userServices.deleteUser(userId);
+        res.status(200).json(user);
+    }catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser
+}
