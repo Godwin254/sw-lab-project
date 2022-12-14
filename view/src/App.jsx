1f9ae5366b6fbd1ef31eeb5fc43831c5633bba9a
Godwin254 <godwinbee15@gmail.com>
2022-12-14 09:15:15 +0300

    feature: add login page

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.scss';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Software Development Lab</h1>
+      
+      <LoginPage />
     </div>
   )
 }
