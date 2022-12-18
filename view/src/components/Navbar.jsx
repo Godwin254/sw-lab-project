2af1c9e49c6d4e69522acf3c399d8a84d54732eb
Godwin254 <godwinbee15@gmail.com>
2022-12-18 20:28:18 +0300

    modify Navbar styles

--- view/src/components/Navbar.jsx
+++ view/src/components/Navbar.jsx
@@ -6,13 +6,41 @@ function Navbar() {
   const handleSignUp = () => {
     alert("under implementation");
   }
+
   return (
     <div className='Navbar'>
         <h1>MindGaurd</h1>
         <nav>
             <a href='#'>Home</a>
             <a href='#'>Explore</a>
-            <a href='#'>Library</a>
+            <div className='explore-dropdown'>
+              <a href='#'>About</a>
+              <a href='#'>Meet up</a>
+              <a href='#'>Programs</a>
+              <a href='#'>Pricing</a>
+            </div>
+            <a href='#' className='library'>Library</a>
+            <div className='lib-dropdown'>
+              <div className='dropdown-category'>
+                <h3>Blogs</h3>
+                <a href='#'>Stress</a>
+                <a href='#'>Anxiety</a>
+                <a href='#'>Depression</a>
+                <a href='#'>Mental health</a>
+              </div>
+              <div className='dropdown-category'>
+                <h3>News</h3>
+                <a href='#'>Daily feeds</a>
+                <a href='#'>National</a>
+                <a href='#'>Communiities</a>
+              </div>
+              <div className='dropdown-category'>
+                <h3>Forums</h3>
+                <a href='#'>Mental Health Awareness</a>
+                <a href='#'>Inua Dada</a>
+                <a href='#'>BoyChild empowerment</a>
+              </div>
+            </div>
         </nav>
 
         <Button handler={handleSignUp} text='Sign up'/>
