92e56a858a255569aea428e50101a18d12b302e5
Godwin254 <godwinbee15@gmail.com>
2022-12-19 11:57:43 +0300

    feat: add Home page and content

---
+++ view/src/components/Section.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Section() {
+  return (
+    <main>
+          <section className='section-1'>
+               
+          </section>
+    </main>
+  )
+}
+
+export default Section
